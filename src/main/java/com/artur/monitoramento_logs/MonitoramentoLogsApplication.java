1a2e9becca048203409dcd98e01b08c97ed368fb
ArturCDA <artur.crispim@academico.ifpb.edu.br>
2026-06-17 15:40:52 -0300

    Primeiro commit

---
+++ src/main/java/com/artur/monitoramento_logs/MonitoramentoLogsApplication.java
@@ -0,0 +1,13 @@
+package com.artur.monitoramento_logs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitoramentoLogsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonitoramentoLogsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/artur/monitoramento_logs/MonitoramentoLogsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artur.monitoramento_logs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitoramentoLogsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
